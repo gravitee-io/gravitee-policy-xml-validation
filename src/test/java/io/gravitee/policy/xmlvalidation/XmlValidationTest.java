d71060e697cc0a1d49885f3aeb1df7fa5979b4c9
Florent CHAMFROY <florent.chamfroy@graviteesource.com>
2026-09-23 11:24:25 +0200

    style: apply the formatting the parent expects
    https://gravitee.atlassian.net/browse/BX-403

--- src/test/java/io/gravitee/policy/xmlvalidation/XmlValidationTest.java
+++ src/test/java/io/gravitee/policy/xmlvalidation/XmlValidationTest.java
@@ -1,11 +1,11 @@
-/**
- * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+/*
+ * Copyright © 2015 The Gravitee team (http://gravitee.io)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *         http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -90,28 +90,28 @@ public class XmlValidationTest {
 
     private Buffer validXmlContent = factory.buffer(
         "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
-        "<root>\n" +
-        "    <companies>\n" +
-        "        <company>\n" +
-        "            <name>Foo Inc</name>\n" +
-        "            <employeeNumber>752</employeeNumber>\n" +
-        "            <sales>10451541505</sales>\n" +
-        "            <CEO>John Doo</CEO>\n" +
-        "        </company>\n" +
-        "    </companies>\n" +
-        "</root>"
+            "<root>\n" +
+            "    <companies>\n" +
+            "        <company>\n" +
+            "            <name>Foo Inc</name>\n" +
+            "            <employeeNumber>752</employeeNumber>\n" +
+            "            <sales>10451541505</sales>\n" +
+            "            <CEO>John Doo</CEO>\n" +
+            "        </company>\n" +
+            "    </companies>\n" +
+            "</root>"
     );
 
     private Buffer invalidXmContent = factory.buffer(
         "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
-        "<root>\n" +
-        "        <company>\n" +
-        "            <name>Foo Inc</name>\n" +
-        "            <employeeNumber>752</employeeNumber>\n" +
-        "            <sales>10451541505</sales>\n" +
-        "            <CEO>John Doo</CEO>\n" +
-        "        </company>\n" +
-        "</root>"
+            "<root>\n" +
+            "        <company>\n" +
+            "            <name>Foo Inc</name>\n" +
+            "            <employeeNumber>752</employeeNumber>\n" +
+            "            <sales>10451541505</sales>\n" +
+            "            <CEO>John Doo</CEO>\n" +
+            "        </company>\n" +
+            "</root>"
     );
 
     private Metrics metrics;
@@ -122,14 +122,13 @@ public class XmlValidationTest {
     public void beforeAll() {
         metrics = Metrics.on(System.currentTimeMillis()).build();
 
-        when(configuration.getErrorMessage())
-            .thenReturn(
-                "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n" +
+        when(configuration.getErrorMessage()).thenReturn(
+            "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n" +
                 "  <error>\n" +
                 "    <reason>validation/internal</reason>\n" +
                 "    <internalReason>Internal error occurred. Please retry...</internalReason>\n" +
                 "  </error>"
-            );
+        );
         when(configuration.getXsdSchema()).thenReturn(xsdSchema);
         when(mockRequest.metrics()).thenReturn(metrics);
         when(mockExecutionContext.getTemplateEngine()).thenReturn(TemplateEngine.templateEngine());
